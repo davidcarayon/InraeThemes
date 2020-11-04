8967caabee09c5fd0a5abe99b0ab375600539cb1
David Carayon <david.carayon@inrae.fr>
2020-11-04 17:06:30 +0100

    added a new report template and new theme based on tidyquant

--- R/scales.R
+++ R/scales.R
@@ -4,15 +4,11 @@
 #' @param ...
 #'
 #' @return
+#' @import scales
 #' @export
 scale_fill_inrae <- function(...){
-  library(scales)
   discrete_scale("fill","inrae",manual_pal(values = c("#00a3a6","#275662","#66c1bf","#008c8e")), ...)
 }
-
-
-
-
 
 #' Title
 #'
@@ -20,58 +16,50 @@ scale_fill_inrae <- function(...){
 #'
 #' @return
 #' @export
+#' @import scales
 scale_color_inrae <- function(...){
-  library(scales)
   discrete_scale("color","inrae",manual_pal(values = c("#00a3a6","#275662","#66c1bf","#008c8e")), ...)
 }
 
 
-
-
-
 #' Title
 #'
 #' @param ...
 #'
 #' @return
-#' @export
-scale_colour_inrae <- function(...){
-  library(scales)
-  discrete_scale("colour","inrae",manual_pal(values = c("#00a3a6","#275662","#66c1bf","#008c8e")), ...)
-}
-
-
-
-
-
-#' Title
-#'
-#' @param ...
-#'
-#' @return
+#' @import scales
 #' @export
 scale_fill_inrae_n5 <- function(...){
-  library(scales)
   discrete_scale("fill","inrae",manual_pal(values = c("#275662","#008C56","#66C1BF","#9ED6E3","#C4C0B3")), ...)
 }
 
-
-
-
-
-
 #' Title
 #'
 #' @param ...
 #'
 #' @return
+#' @import scales
 #' @export
 scale_color_inrae_n5 <- function(...){
-  library(scales)
   discrete_scale("color","inrae",manual_pal(values = c("#275662","#008C56","#66C1BF","#9ED6E3","#C4C0B3")), ...)
 }
 
-
+palette_light <- function() {
+  c(
+    blue         = "#2c3e50", # blue
+    red          = "#e31a1c", # red
+    green        = "#18BC9C", # green
+    yellow       = "#CCBE93", # yellow
+    steel_blue   = "#a6cee3", # steel_blue
+    navy_blue    = "#1f78b4", # navy_blue
+    light_green  = "#b2df8a", # light_green
+    pink         = "#fb9a99", # pink
+    light_orange = "#fdbf6f", # light_orange
+    orange       = "#ff7f00", # orange
+    light_purple = "#cab2d6", # light_purple
+    purple       = "#6a3d9a"  # purple
+  ) %>% toupper()
+}
 
 
 #' Title
@@ -79,8 +67,25 @@ scale_color_inrae_n5 <- function(...){
 #' @param ...
 #'
 #' @return
+#' @import scales ggplot2
 #' @export
-scale_colour_inrae_n5 <- function(...){
-  library(scales)
-  discrete_scale("colour","inrae",manual_pal(values = c("#275662","#008C56","#66C1BF","#9ED6E3","#C4C0B3")), ...)
+scale_color_quant <- function(...) {
+
+  pal <- rep(unname(palette_light()), 100)
+
+  scale_color_manual(values = pal)
+}
+
+#' Title
+#'
+#' @param ...
+#'
+#' @return
+#' @import scales ggplot2
+#' @export
+scale_fill_quant <- function(...) {
+
+  pal <- rep(unname(palette_light()), 100)
+
+  scale_fill_manual(values = pal)
 }
--- R/themes.R
+++ R/themes.R
@@ -153,5 +153,71 @@ theme_inrae_1 <- function(base_size = 10, base_family = "Avenir Next Pro") {
     )
 }
 
+#' Title
+#'
+#' @param base_size base size
+#' @param base_family base family
+#' @import ggplot2
+#'
+#' @return
+#' @export
+theme_quant <- function(base_size = 15, base_family = "Avenir Next Pro") {
+
+  # Tidyquant colors
+  blue  <- "#2c3e50"
+  green <- "#18BC9C"
+  white <- "#FFFFFF"
+  grey  <- "grey80"
+
+  # Starts with theme_grey and then modify some parts
+  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+
+    ggplot2::theme(
+
+      # Base Inherited Elements
+      line               =  ggplot2::element_line(colour = blue, size = 0.5, linetype = 1,
+                                                  lineend = "butt"),
+      rect               =  ggplot2::element_rect(fill = white, colour = blue,
+                                                  size = 0.5, linetype = 1),
+      text               =  ggplot2::element_text(family = base_family, face = "plain",
+                                                  colour = blue, size = base_size,
+                                                  lineheight = 0.9, hjust = 0.5, vjust = 0.5, angle = 0,
+                                                  margin = ggplot2::margin(), debug = FALSE),
+
+      # Axes
+      axis.line          = ggplot2::element_blank(),
+      axis.text          = ggplot2::element_text(size = rel(0.8)),
+      axis.ticks         = ggplot2::element_line(color = grey, size = rel(1/3)),
+      axis.title         = ggplot2::element_text(size = rel(1.0), face = "bold"),
+
+      # Panel
+      panel.background   = ggplot2::element_rect(fill = white, color = NA),
+      panel.border       = ggplot2::element_rect(fill = NA, size = rel(1/2), color = blue),
+      panel.grid.major   = ggplot2::element_line(color = grey, size = rel(1/3)),
+      panel.grid.minor   = ggplot2::element_line(color = grey, size = rel(1/3)),
+      panel.grid.minor.x = ggplot2::element_blank(),
+      panel.spacing      = ggplot2::unit(.75, "cm"),
+
+      # Legend
+      legend.key         = ggplot2::element_rect(fill = white, color = NA),
+      legend.position    = "top",
+      legend.title = element_text(face = "bold"),
+
+      # Strip (Used with multiple panels)
+      strip.background   = ggplot2::element_rect(fill = blue, color = blue),
+      strip.text         = ggplot2::element_text(color = white, size = ggplot2::rel(0.8), margin = ggplot2::margin(t = 5, b = 5)),
+
+      # Plot
+      plot.title         = ggplot2::element_text(size = rel(1.2), hjust = 0,
+                                                 margin = ggplot2::margin(t = 0, r = 0, b = 4, l = 0, unit = "pt")),
+      plot.subtitle      = ggplot2::element_text(size = rel(0.9), hjust = 0,
+                                                 margin = ggplot2::margin(t = 0, r = 0, b = 3, l = 0, unit = "pt")),
+
+      # Complete theme
+      complete = TRUE
+    )
+}
+
+
 
 
